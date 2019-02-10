70fe3f06b9f000dff05f2407a2b981138e7a448b
will <wrice20@gmail.com>
2019-02-10 15:59:41 -0500

    added soil peplinski

--- generate_training_data.py
+++ generate_training_data.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 
 X = 1.0
 Y = 1.0
+Z = 0.1
 CYLINDER_X=0.33
 CYLINDER_Y=0.33
 
@@ -21,157 +22,130 @@ def blockPrint():
 
 
 pipe_materials = ['pec', 'pvc', 'concrete']
-ground_materials = ['sand', 'saturated_sand', 'dirt', 'clay', 'mixed']
+# ground materials are what the primary component of peplinksi mixture is
+
+sand_fraction = np.arange(start=0.1,
+                          stop=0.9,
+                          step=0.1)
+clay_fraction = np.arange(start=0.1,
+                          stop=0.9,
+                          step=0.1)
 
 diameters = np.arange(start=0.01,
                       stop=0.1,
                       step=0.01)
 
 for i in tqdm(range(1)):
+    seed = np.random.randint(1)
 
     blockPrint()
     for pipe_material in pipe_materials:
 
-        for ground in ground_materials:
+        for sand in sand_fraction:
 
-            for diameter in diameters:
+            for clay in reversed(clay_fraction):
 
-                domain = command(
-                    'domain', X, Y, 0.002)
+                for diameter in diameters:
 
-                dx_dy_dz = command(
-                    'dx_dy_dz', 0.002, 0.002, 0.002)
+                    domain = command(
+                        'domain', X, Y, Z)
 
-                time_window = command(
-                    'time_window', 12e-9)
+                    dx_dy_dz = command(
+                        'dx_dy_dz', 0.002, 0.002, 0.002)
 
-                if ground is 'saturated_sand':
-                    material = command(
-                        'material', 20, 0.1,
-                        1.0, 0.0, 'ground')
+                    time_window = command(
+                        'time_window', 12e-9)
+
+                    soil_peplinski = command(
+                        'soil_peplinski', sand, clay, 2.0,
+                        2.66, 0.001, 0.25, 'my_soil'
+                    )
                     box = command(
-                    'box', 0.0, 0.0, 0.0, X,
-                    Y*.75, 0.002, 'ground')
-                elif ground is 'dirt':
-                    material = command(
-                        'material', 5, 0.1,
-                        1.0, 0.0, 'ground')
-                    box = command(
-                        'box', 0.0, 0.0, 0.0, X,
-                    Y*.75, 0.002, 'ground')
-                elif ground is 'clay':
-                    material = command(
-                        'material', 10, 0.1,
-                        1.0, 0.0, 'ground')
-                    box = command(
-                    'box', 0.0, 0.0, 0.0, X,
-                    Y*.75, 0.002, 'ground')
-                elif ground is 'sand':
-                    material = command(
-                        'material', 3, 0.01,
-                        1.0, 0.0, 'ground')
-                    box = command(
-                    'box', 0.0, 0.0, 0.0, X,
-                    Y*.75, 0.002, 'ground')
-                else:
-                    sat_sand = command(
-                        'material', 20, 0.1,
-                        1.0, 0.0, 'saturated_sand')
-                    dirt = command(
-                        'material', 5, 0.1,
-                        1.0, 0.0, 'dirt')
-                    clay = command(
-                        'material', 10, 0.1,
-                        1.0, 0.0, 'clay')
-                    sand = command(
-                        'material', 3, 0.01,
-                        1.0, 0.0, 'sand')
-                    box = command(
-                    'box', 0.0, 0.0, 0.0, X,
-                    Y*.75, 0.002, 'saturated_sand')
-                     
-                
+                        'fractal_box', 0, 0, 0, 1,
+                        0.75, 0.1, 1.5, 1, 1, 1, 50,
+                        'my_soil', 'my_soil_box', seed)
+                    
+                    roughness = command(
+                        'add_surface_roughness', 0, 0, 0.1,
+                        0.1, 0.1, 0.1, 1.5, 1, 1, 0.065, 0.080,
+                        'my_soil_box'
+                    )
 
-                if pipe_material is 'pvc':
-                    pvc = command(
-                        'material', 3, 0,
-                        1, 0, 'pvc')
-                    cylinder = command(
-                        'cylinder', CYLINDER_X, 0.33, 0, CYLINDER_X,
-                        0.33, 0.002, diameter, 'pvc', 'y')
-                elif pipe_material is 'pec':
-                    cylinder = command(
-                        'cylinder', CYLINDER_X, 0.33, 0, CYLINDER_X,
-                        0.33, 0.002, diameter, 'pec', 'y')
-                else:
-                    concrete =  pvc = command(
-                        'material', 6, 0,
-                        1, 0, 'concrete')
-                    cylinder = command(
-                        'cylinder', CYLINDER_X, 0.33, 0, CYLINDER_X,
-                        0.33, 0.002, diameter, 'concrete', 'y')
-
-                rx = command(
-                    'rx', 0.1125, 0.1525, 0)
-
-                src_steps = command(
-                    'src_steps', 0.002, 0.0, 0)
-
-                rx_steps = command(
-                    'rx_steps', 0.002, 0.0, 0)
-
-                waveform = command(
-                    'waveform', 'ricker', 1,
-                    900e6, 'my_ricker')
-
-                hertzian_dipole = command(
-                    'hertzian_dipole', 'z', 0.150,
-                    0.170, 0, 'my_ricker')
-
-                geometry_view = command(
-                    'geometry_view', 0, 0,
-                    0, X, Y, 0.002, 0.002,
-                    0.002, 0.002, 'cylinder_half_space',
-                    'n')
-
-                message = command('messages', 'n')
-
-                with open(
-                    os.path.join(
-                    'input-files',
-                    'cylinder_Bscan_2D_{}_{}_{}_{}.in'.format(
-                    i,
-                    pipe_material,
-                    np.round(diameter, decimals=4),
-                    ground)), 'w') as f:
-                    f.write(domain+'\n')
-                    f.write(dx_dy_dz+'\n')
-                    f.write(time_window+'\n')
-                    f.write(material+'\n')
+                    
 
                     if pipe_material is 'pvc':
-                        f.write(pvc+'\n')
-                    elif pipe_material is 'concrete':
-                        f.write(concrete+'\n')
+                        pvc = command(
+                            'material', 3, 0,
+                            1, 0, 'pvc')
+                        cylinder = command(
+                            'cylinder', CYLINDER_X, 0.33, 0, CYLINDER_X,
+                            0.33, Z, diameter, 'pvc', 'y')
+                    elif pipe_material is 'pec':
+                        cylinder = command(
+                            'cylinder', CYLINDER_X, 0.33, 0, CYLINDER_X,
+                            0.33, Z, diameter, 'pec', 'y')
                     else:
-                        pass
+                        concrete =  pvc = command(
+                            'material', 6, 0,
+                            1, 0, 'concrete')
+                        cylinder = command(
+                            'cylinder', CYLINDER_X, 0.33, 0, CYLINDER_X,
+                            0.33, Z, diameter, 'concrete', 'y')
 
-                    if ground is 'mixed':
-                        f.write(sat_sand+'\n')
-                        f.write(dirt+'\n')
-                        f.write(clay+'\n')
-                        f.write(sand+'\n')
-                        f.write(box+' dirt' + ' clay'+'\n')
-                    else:
+                    rx = command(
+                        'rx', 0.1125, 0.1525, 0)
+
+                    src_steps = command(
+                        'src_steps', 0.002, 0.0, 0)
+
+                    rx_steps = command(
+                        'rx_steps', 0.002, 0.0, 0)
+
+                    waveform = command(
+                        'waveform', 'ricker', 1,
+                        900e6, 'my_ricker')
+
+                    hertzian_dipole = command(
+                        'hertzian_dipole', 'z', 0.150,
+                        0.170, 0, 'my_ricker')
+
+                    geometry_view = command(
+                        'geometry_view', 0, 0,
+                        0, X, Y, 0.002, 0.002,
+                        0.002, 0.002, 'cylinder_half_space',
+                        'n')
+
+                    message = command('messages', 'n')
+
+                    with open(
+                        os.path.join(
+                        'input-files',
+                        'cylinder_Bscan_2D_{}_{}_{}_{}.in'.format(
+                        i,
+                        pipe_material,
+                        np.round(diameter, decimals=4),
+                        sand)), 'w') as f:
+                        f.write(domain+'\n')
+                        f.write(dx_dy_dz+'\n')
+                        f.write(time_window+'\n')
+                        f.write(soil_peplinski+'\n')
                         f.write(box+'\n')
+                        f.write(roughness+'\n')
 
-                    f.write(cylinder+'\n')
-                    f.write(rx+'\n')
-                    f.write(src_steps+'\n')
-                    f.write(rx_steps+'\n')
-                    f.write(waveform+'\n')
-                    f.write(hertzian_dipole+'\n')
-                    #f.write(geometry_view+'\n')
-                    f.write(message)
+                        if pipe_material is 'pvc':
+                            f.write(pvc+'\n')
+                        elif pipe_material is 'concrete':
+                            f.write(concrete+'\n')
+                        else:
+                            pass
+                        
+                        f.write(cylinder+'\n')
+                        f.write(rx+'\n')
+                        f.write(src_steps+'\n')
+                        f.write(rx_steps+'\n')
+                        f.write(waveform+'\n')
+                        f.write(hertzian_dipole+'\n')
+                        #f.write(geometry_view+'\n')
+                        f.write(message)
 
-Generator(n_scans=300, in_dir='input-files/*.in', out_dir='output-files/')
+Generator(n_scans=200, in_dir='input-files/*.in', out_dir='output-files/')
